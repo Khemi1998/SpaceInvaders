c7a91b698446f1b60e83ab5af9101bf86334afb2
Khemi <khemi@Khemis-Laptop.local>
2021-11-17 00:35:25 +0000

    iteration5: activate game using play button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //constants
 const grid = document.querySelector(".game-grid");
+const play = document.querySelector(".gameAction")
+
 const position = grid.childNodes;
 
 //grid_square is a node list
@@ -34,58 +36,57 @@ entity = position[entity_postion].classList.add("entity");
 
 
 
-//document detect when arrows are pressed and runs function to move enitity
-document.onkeydown = detectArrow;
 
-//moving the entity
-function detectArrow(e) {
-    entity = position[entity_postion].classList.remove("entity");
-    //stays on bottom line 
 
-    switch (e.key) {
-        case ("ArrowLeft") :
-            //left arrow
-            if (entity_postion !=99){
-                entity_postion = entity_postion-1;
-                entity = position[entity_postion].classList.add("entity");
-            } else {
-                entity = position[entity_postion].classList.add("entity");
-            }
-        break;
-        case ("ArrowRight") :
-            //right arrow
-            if (entity_postion != 109){
-                entity_postion = entity_postion+1;
-                entity = position[entity_postion].classList.add("entity"); 
-            }else {
-                entity = position[entity_postion].classList.add("entity");
-            }
-            break;
-        case ("ArrowUp") :
-            //up arrow
-            //shooting
-            entity = position[entity_postion].classList.add("entity");
+//game activation by pressing play
+play.addEventListener ("click",() => {
 
-            //laser movement 100ms speed
-            for (let i = 1; i < 10; i++) {
-                (function(i) {
-                    setTimeout(function() { 
-                    position[laser].classList.remove("laser");
-                    laser = entity_postion -11*[i];
-                    position[laser].classList.add("laser"); 
-                }, 100 * i);
-                })(i);
+    //document detect when arrows are pressed and runs function to move enitity
+    document.onkeydown = detectArrow;
+
+
+    //moving the entity
+    function detectArrow(e) {
+        entity = position[entity_postion].classList.remove("entity");
+        //stays on bottom line 
+        switch (e.key) {
+            case ("ArrowLeft") :
+                //left arrow
+                if (entity_postion !=99){
+                    entity_postion = entity_postion-1;
+                    entity = position[entity_postion].classList.add("entity");
+                } else {
+                    entity = position[entity_postion].classList.add("entity");
                 }
-
-                if (laser<=10){
-                    position[laser].classList.remove("laser");
-                    console.log("working")
-                }
-            
-        break;
-        default:
-            entity = position[entity_postion].classList.add("entity");
             break;
-    }
-}
+            case ("ArrowRight") :
+                //right arrow
+                if (entity_postion != 109){
+                    entity_postion = entity_postion+1;
+                    entity = position[entity_postion].classList.add("entity"); 
+                }else {
+                    entity = position[entity_postion].classList.add("entity");
+                }
+                break;
+            case ("ArrowUp") :
+                //up arrow
+                //shooting
+                entity = position[entity_postion].classList.add("entity");
 
+                //laser movement 200ms
+                for (let i = 1; i <= 9; i++) {
+                    (function(i) {
+                        setTimeout(function() { 
+                        position[laser].classList.remove("laser");
+                        laser = entity_postion -11*[i];
+                        position[laser].classList.add("laser"); 
+                        
+                    }, 200 * i);
+                    })(i);
+                    } 
+            break;
+            default:
+                entity = position[entity_postion].classList.add("entity");
+                break;
+        }
+    }})
