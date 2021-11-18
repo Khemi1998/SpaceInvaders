249942694e5ec298afcca83c52f0c5897f7399c8
Khemi <khemi@Khemis-Laptop.local>
2021-11-18 09:50:42 +0000

    iteration7: function method instead of for loop for laser

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let grid_square = [];
 let entity;
 let laser = 104;
 let entity_postion = 104;
-let invaderMovement;
+let incrementer = 0;
 
 //setting up game grid
 for (let i=0; i < 110; i++){
@@ -35,14 +35,37 @@ for (let i=0; i < invadersArr.length; i++){
 //default position bottom middle 
 entity = position[entity_postion].classList.add("entity");
 
-
+let deadInvaders = [];
 //--------------------------------------------------------------------------------------
+
 
 
 //game activation by pressing play
 play.addEventListener ("click",() => {
-
+    
     //invaders movement
+/*
+    function _invaderMovement() {
+    for(let j=0; j<84; j++){
+        position[j].classList.remove("invaders");
+        position[11+j].classList.remove("invaders");
+        position[22+j].classList.remove("invaders");
+    }}
+*/
+    function test() {
+            //_invaderMovement()
+            let i=0; 
+            //i<84; 
+            i++;       
+            invadersArr.forEach((invader)=>{
+            invader = invader + i;
+            position[invader].classList.add("invaders");
+    })}
+
+    setInterval(test,100)
+
+    /*
+    METHOD1 DOESNT WORK
     for(let i=0; i<84; i++){
         (function(i) {
             setTimeout(function() {  
@@ -58,7 +81,8 @@ play.addEventListener ("click",() => {
     })
     }, 100 * i);
 })(i);
-}
+}*/
+
     //document detect when arrows are pressed and runs function to move enitity
     document.onkeydown = detectArrow;
 
@@ -85,29 +109,56 @@ play.addEventListener ("click",() => {
                     entity = position[entity_postion].classList.add("entity");
                 }
                 break;
-            case ("ArrowUp") :
-                //up arrow
-                //shooting
-                entity = position[entity_postion].classList.add("entity");
-                
-                //laser movement 200ms
-                for (let i = 1; i <= 9; i++) {
-                    (function(i) {
-                        setTimeout(function() { 
-                        position[laser].classList.remove("laser");
-                        laser = entity_postion -11*[i];
-                        position[laser].classList.add("laser");                      
-                    }, 200 * i);
-                    })(i);
-                    }
-            break;
-            default:
-                entity = position[entity_postion].classList.add("entity");
-                break;
-        }
-    }})
+                case ("ArrowUp") :
+                    //up arrow
+                    //shooting
+                    laser=entity_postion;
 
-    //collision
-if (grid_square.classList.contains("laser") & (grid_square.classList.contains("invaders"))) {
-    alert("yay");
-}
+                    entity = position[entity_postion].classList.add("entity");
+
+                    function _laser() {
+                        position[laser].classList.remove("laser");
+                        laser -=11;
+                        position[laser].classList.add("laser");
+
+                        //collision
+                        if (position[laser].classList.contains("invaders")){
+                            position[laser].classList.remove("laser");
+                            //stops lasers
+                            clearInterval(laserGone);
+                        }
+                    } 
+                    //speed 100ms
+                    const laserGone =setInterval(_laser,100)
+
+                    /*
+                    MEDTHOD1 DOESNT WORK
+                      //laser movement 200ms
+                      laser=entity_postion;
+
+                    for (let i = 1; (i <= 9); i++) {
+                        (function(i) {
+                            setTimeout(function() { 
+
+                            position[laser].classList.remove("laser");
+                            laser -=11;
+                            position[laser].classList.add("laser");
+                            
+                           // console.log (position[laser])
+                            
+                            //collisions
+                            if (position[laser].classList.contains("invaders")){
+                               position[laser].classList.remove("laser");
+                               laser=null;
+                            }
+                        }, 100 * i); 
+                        })(i);                    
+                    }*/
+                    
+                break;
+                default:
+                    entity = position[entity_postion].classList.add("entity");
+                    break;
+        }  
+
+    }})
