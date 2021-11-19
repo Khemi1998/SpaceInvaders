87c9910ff079dce9aaad6eb3757cf1063287e928
Khemi <khemi@Khemis-Laptop.local>
2021-11-19 15:37:37 +0000

    iteration11: working version

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ for (let i=0; i < 110; i++){
 }
 
 //setting up the invaders positions
-const invadersToparr = [
-    0, 1, 2, 3, 4
-];
+//constantly changing
+let invadersToparr = [ 0, 1, 2, 3, 4];
+let invadersMiddlearr = [11, 12, 13, 14, 15];
+let invadersBottomarr = [22, 23, 24, 25, 26];
 
 //assigning invaders to grid function 
 const showInvader =invadersArr => {
@@ -32,6 +33,8 @@ const showInvader =invadersArr => {
 }}
 
 showInvader(invadersToparr);
+showInvader(invadersMiddlearr);
+showInvader(invadersBottomarr);
 
 //new invader position function
 const newPosition = invadersArr => {
@@ -49,6 +52,7 @@ const newPosition = invadersArr => {
     invadersArr.push(lastRowinvader+1);
 
 }
+
 
 //assigning enitity to grid
 //default position bottom middle 
@@ -72,44 +76,20 @@ play.addEventListener ("click",() => {
         newPosition(invadersToparr);
         showInvader(invadersToparr);
 
+        newPosition(invadersMiddlearr);
+        showInvader(invadersMiddlearr);
 
-        /* LONG WINDED METHOD
-        position[incrementer].classList.remove("invaders");
-        position[11+incrementer].classList.remove("invaders");
-        position[22+incrementer].classList.remove("invaders");
-        
-        position[5+incrementer].classList.add("invaders");
-        position[16+incrementer].classList.add("invaders");
-        position[27+incrementer].classList.add("invaders");
-        incrementer++;
-        */
+        newPosition(invadersBottomarr);
+        showInvader(invadersBottomarr);
 
         if (position[109].classList.contains("invaders")) {
             clearInterval(invaders);
             result.innerHTML="YOU LOSE!"
         }
     }
-
-    const invaders = setInterval(invaderMove, 200);
-/*
-    /*
-    METHOD1 DOESNT WORK
-    for(let i=0; i<84; i++){
-        (function(i) {
-            setTimeout(function() {  
-        for(let j=0; j<84; j++){
-        position[j].classList.remove("invaders");
-        position[11+j].classList.remove("invaders");
-        position[22+j].classList.remove("invaders");
-    }
+    //invader speed
+    const invaders = setInterval(invaderMove, 100);
     
-    invadersArr.forEach((invader)=>{
-        invader=invader + i;
-        position[invader].classList.add("invaders");
-    })
-    }, 100 * i);
-})(i);
-}*/
 
     //document detect when arrows are pressed and runs function to move enitity
     document.onkeydown = detectArrow;
@@ -139,76 +119,64 @@ play.addEventListener ("click",() => {
                     entity = position[entity_postion].classList.add("entity");
                 }
                 break;
-                case ("ArrowUp") :
-                    //up arrow
-                    //shooting
-                    laser=entity_postion;
+      case "ArrowUp":
+        //up arrow
+        //shooting
+        laser = entity_postion;
 
-                    entity = position[entity_postion].classList.add("entity");
+        entity = position[entity_postion].classList.add("entity");
 
-                    function _laser() {
-                        position[laser].classList.remove("laser");
-                        laser -=11;
-                        if (laser>11){
-                        position[laser].classList.add("laser");
-                        }
-                        else{
-                            clearInterval(laserGone);
-                        }
+        function _laser() {
+          position[laser].classList.remove("laser");
+          laser -= 11;
+          if (laser > 11) {
+            position[laser].classList.add("laser");
+          } else {
+            clearInterval(laserGone);
+          }
 
-                        //collision
-                        //stops lasers
-                        if (position[laser].classList.contains("invaders")){
-                            position[laser].classList.remove("laser");
-                            clearInterval(laserGone);
-                            score++;
-                            /*
-                            deads.forEach((dead)=>{
-                                position[dead+incrementer].classList.remove("invaders");
-                                incrementer++
-                            })*/
-                        }
+          //collision
+          //stops lasers
+          
+          if (position[laser].classList.contains("invaders")) {
+            console.log(laser);
 
-                        if (score===14){
-                            result.innerHTML="YOU WON!"
-                        }
-                      
-                    } 
-                    //speed 100ms
-                    const laserGone = setInterval(_laser,25)
+            //remove dead invader from array
+            if (invadersToparr.includes(laser)) {
+                invadersToparr = invadersToparr.filter((invader) =>{ return invader != laser});
 
-                    //gets rid of lingering laser classlists
-                        position.forEach((square)=>{
-                        position[laser].classList.remove("laser");
-                    })
-                    /*
-                    MEDTHOD1 DOESNT WORK
-                      //laser movement 200ms
-                      laser=entity_postion;
-                    for (let i = 1; (i <= 9); i++) {
-                        (function(i) {
-                            setTimeout(function() { 
-                            position[laser].classList.remove("laser");
-                            laser -=11;
-                            position[laser].classList.add("laser");
-                            
-                           // console.log (position[laser])
-                            
-                            //collisions
-                            if (position[laser].classList.contains("invaders")){
-                               position[laser].classList.remove("laser");
-                               laser=null;
-                            }
-                        }, 100 * i); 
-                        })(i);                    
-                    }*/
-                    
-                break;
-                default:
-                    entity = position[entity_postion].classList.add("entity");
-                    break;
-        }  
+            } else if (invadersMiddlearr.includes(laser)) {
+                invadersMiddlearr = invadersMiddlearr.filter((invader) =>{ return invader != laser});
 
-    }})
+            } else {
+                invadersBottomarr = invadersBottomarr.filter((invader) =>{ return invader != laser});
+            }
 
-    //----------------------------------------------------------------------------
+            position[laser].classList.remove("invaders");
+            position[laser].classList.remove("laser");
+            clearInterval(laserGone);
+            score++;
+
+          }
+
+          if (score === 15) {
+            result.innerHTML = "YOU WON!";
+          }
+        }
+        //speed 100ms
+        const laserGone = setInterval(_laser, 100);
+
+        //gets rid of lingering laser classlists
+        position.forEach(square => {
+          position[laser].classList.remove("laser");
+        });
+
+        break;
+      default:
+        entity = position[entity_postion].classList.add("entity");
+        break;
+    }
+  }
+});
+
+//----------------------------------------------------------------------------
