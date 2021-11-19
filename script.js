7744c868a03a8d3d1bc2ece1b07fe5cce21edd7f
Khemi <khemi@Khemis-Laptop.local>
2021-11-19 18:07:36 +0000

    iteration12: fixed bugs and user loses if entity crashes

--- script.js
+++ script.js
@@ -10,121 +10,118 @@ const position = grid.childNodes;
 let grid_square = [];
 let laser = 104;
 let entity_postion = 104;
+let incrementer = 0;
 let score = 0;
 
 //setting up game grid
-for (let i=0; i < 110; i++){
-    grid_square = document.createElement("div");
+for (let i = 0; i < 110; i++) {
+  grid_square = document.createElement("div");
 
-    grid.appendChild(grid_square);
-
+  grid.appendChild(grid_square);
 }
 
 //setting up the invaders positions
-//constantly changing
-let invadersToparr = [ 0, 1, 2, 3, 4];
-let invadersMiddlearr = [11, 12, 13, 14, 15];
-let invadersBottomarr = [22, 23, 24, 25, 26];
+let invadersTopArray = [0, 1, 2, 3, 4];
+let invadersMiddleArray = [11, 12, 13, 14, 15];
+let invadersBottomArray = [22, 23, 24, 25, 26];
 
-//assigning invaders to grid function 
-const showInvader =invadersArr => {
-    for (let i=0; i < invadersArr.length; i++){
+//assigning invaders to grid
+const showInvader = invadersArr => {
+  for (let i = 0; i < invadersArr.length; i++) {
     position[invadersArr[i]].classList.add("invaders");
-}}
+  }
+};
 
-showInvader(invadersToparr);
-showInvader(invadersMiddlearr);
-showInvader(invadersBottomarr);
+showInvader(invadersTopArray);
+showInvader(invadersMiddleArray);
+showInvader(invadersBottomArray);
 
-//new invader position function
-const newPosition = invadersArr => {
+//invaders movement
+const moveArray = invadersArr => {
 
-    //remove first row fo array
-    let firstRowinvader = invadersArr.shift();
+    //declaring the last invader in each row
+    let lastNumber = invadersArr[invadersArr.length - 1];
 
-    //remove class of next position
-    position[firstRowinvader].classList.remove("invaders");
-    
-    //declare last invader
-    let lastRowinvader = invadersArr[invadersArr.length-1];
+    //declaring the first invader in each row and  removing them from array
+    let firstItem = invadersArr.shift();
 
-    //move along
-    invadersArr.push(lastRowinvader+1);
+    //removing class invaders from the previous first row invader
+    position[firstItem].classList.remove("invaders");
 
-}
-
+    //adding new invader at the end or array row to simulate movement
+    invadersArr.push(lastNumber + 1);
+  };
 
 //assigning enitity to grid
-//default position bottom middle 
-position[entity_postion].classList.add("entity");
+//default position bottom middle
+entity = position[entity_postion].classList.add("entity");
 
 //restart
-refresh.addEventListener("click", ()=> {
-location.reload();
-})
+refresh.addEventListener("click", () => {
+  location.reload();
+});
 
 //--------------------------------------------------------------------------------------
 
-
 //game activation by pressing play
-play.addEventListener ("click",() => {
-    play.style.display = "none";
-    refresh.style.display = "block";   
+play.addEventListener("click", () => {
+  play.style.display = "none";
+  refresh.style.display = "block";
 
-    //invaders movement
-    function invaderMove() {
-        newPosition(invadersToparr);
-        showInvader(invadersToparr);
-
-        newPosition(invadersMiddlearr);
-        showInvader(invadersMiddlearr);
-
-        newPosition(invadersBottomarr);
-        showInvader(invadersBottomarr);
-
-        if (position[109].classList.contains("invaders")) {
-            clearInterval(invaders);
-            result.innerHTML="YOU LOSE!"
-        }
-    }
-    //invader speed
-    const invaders = setInterval(invaderMove, 100);
+  function invaderMove() {
+      moveArray(invadersTopArray);
+      showInvader(invadersTopArray);
     
+      moveArray(invadersMiddleArray);
+      showInvader(invadersMiddleArray);
+    
+      moveArray(invadersBottomArray);
+      showInvader(invadersBottomArray);
+    
+    //entity crashes into invader and explodes
+    if (position[entity_postion].classList.contains("invaders")) {
+      clearInterval(invaders);
+      position[entity_postion].classList.remove("invaders");
+      position[entity_postion].classList.add("boom")
+      result.innerHTML="YOU LOSE!"
+    }
+  }
 
-    //document detect when arrows are pressed and runs function to move enitity
-    document.onkeydown = detectArrow;
+  const invaders = setInterval(invaderMove, 200);
 
-    //moving the entity
-    function detectArrow(e) {
+  //document detect when arrows are pressed and runs function to move enitity
+  document.onkeydown = detectArrow;
 
-        entity = position[entity_postion].classList.remove("entity");
-        
-        //stays on bottom line 
-        switch (e.key) {
-            case ("ArrowLeft") :
-                //left arrow
-                if (entity_postion !=99){
-                    entity_postion = entity_postion-1;
-                    entity = position[entity_postion].classList.add("entity");
-                } else {
-                    entity = position[entity_postion].classList.add("entity");
-                }
-            break;
-            case ("ArrowRight") :
-                //right arrow
-                if (entity_postion != 109){
-                    entity_postion = entity_postion+1;
-                    entity = position[entity_postion].classList.add("entity"); 
-                }else {
-                    entity = position[entity_postion].classList.add("entity");
-                }
-                break;
+  //moving the entity
+  function detectArrow(e) {
+    position[entity_postion].classList.remove("entity");
+
+    //stays on bottom line
+    switch (e.key) {
+      case "ArrowLeft":
+        //left arrow
+        if (entity_postion != 99) {
+          entity_postion = entity_postion - 1;
+          position[entity_postion].classList.add("entity");
+        } else {
+          position[entity_postion].classList.add("entity");
+        }
+        break;
+      case "ArrowRight":
+        //right arrow
+        if (entity_postion != 109) {
+          entity_postion = entity_postion + 1;
+          entity = position[entity_postion].classList.add("entity");
+        } else {
+          entity = position[entity_postion].classList.add("entity");
+        }
+        break;
       case "ArrowUp":
         //up arrow
         //shooting
         laser = entity_postion;
 
-        entity = position[entity_postion].classList.add("entity");
+        position[entity_postion].classList.add("entity");
 
         function _laser() {
           position[laser].classList.remove("laser");
@@ -137,26 +134,25 @@ play.addEventListener ("click",() => {
 
           //collision
           //stops lasers
-          
           if (position[laser].classList.contains("invaders")) {
             console.log(laser);
 
-            //remove dead invader from array
-            if (invadersToparr.includes(laser)) {
-                invadersToparr = invadersToparr.filter((invader) =>{ return invader != laser});
 
-            } else if (invadersMiddlearr.includes(laser)) {
-                invadersMiddlearr = invadersMiddlearr.filter((invader) =>{ return invader != laser});
-
-            } else {
-                invadersBottomarr = invadersBottomarr.filter((invader) =>{ return invader != laser});
+            if (invadersTopArray.includes(laser)) {
+                invadersTopArray = invadersTopArray.filter(invader => invader != laser);
             }
-
+            
+             else if (invadersMiddleArray.includes(laser)) {
+                invadersMiddleArray = invadersMiddleArray.filter(invader => invader != laser);
+            } 
+            
+            else {
+                invadersBottomArray = invadersBottomArray.filter(invader => invader != laser);
+            }
             position[laser].classList.remove("invaders");
             position[laser].classList.remove("laser");
             clearInterval(laserGone);
             score++;
-
           }
 
           if (score === 15) {
@@ -164,17 +160,11 @@ play.addEventListener ("click",() => {
           }
         }
         //speed 100ms
-        const laserGone = setInterval(_laser, 100);
-
-        //gets rid of lingering laser classlists
-        position.forEach(square => {
-          position[laser].classList.remove("laser");
-        });
-
-        break;
-      default:
-        entity = position[entity_postion].classList.add("entity");
-        break;
+        const laserGone = setInterval(_laser, 20);
+     break;
+        default:
+        position[entity_postion].classList.add("entity");
+     break;
     }
   }
 });
